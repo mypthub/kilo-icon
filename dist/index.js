fa1b75144a2ea72d61d8202bace6b1787ca7bae1
Denis Gruba <denis.gruba@mypthub.net>
2019-01-15 15:53:27 +0000

    Change icon font generator to build ligatures

--- dist/index.js
+++ dist/index.js
@@ -1,4 +1,15 @@
-import json from './icon-font/icons.json';
-import ttf from './icon-font/icons.ttf';
+import json from './icon-font/kilo-icons.json';
+import ttf from './icon-font/kilo-icons.ttf';
+import eot from './icon-font/kilo-icons.eot';
+import woff from './icon-font/kilo-icons.woff';
+import woff2 from './icon-font/kilo-icons.woff2';
+import svg from './icon-font/kilo-icons.svg';
 
-export { json as kiloIconJSON, ttf as kiloIconTTF };
+export {
+  json as kiloIconJSON,
+  ttf as kiloIconTTF,
+  eot as kiloIconEOT,
+  woff as kiloIconWOFF,
+  woff2 as kiloIconWOFF2,
+  svg as kiloIconSVG,
+};
