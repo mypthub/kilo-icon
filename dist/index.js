8a6d678bccf8014cc8522526840c5060335d85cf
Denis Gruba <denis.gruba@mypthub.net>
2019-01-15 17:10:56 +0000

    Remove svg import

--- dist/index.js
+++ dist/index.js
@@ -4,7 +4,6 @@ import ttf from './icon-font/kilo-icons.ttf';
 import eot from './icon-font/kilo-icons.eot';
 import woff from './icon-font/kilo-icons.woff';
 import woff2 from './icon-font/kilo-icons.woff2';
-import svg from './icon-font/kilo-icons.svg';
 
 export {
   json as kiloIconJSON,
@@ -13,5 +12,4 @@ export {
   eot as kiloIconEOT,
   woff as kiloIconWOFF,
   woff2 as kiloIconWOFF2,
-  svg as kiloIconSVG,
 };
