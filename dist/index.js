fe303fa969af91bcf47ca92fbfe77b8d1f384869
Denis Gruba <denis.gruba@mypthub.net>
2019-01-16 11:36:15 +0000

    Add a separate ligature generator

--- dist/index.js
+++ dist/index.js
@@ -4,6 +4,12 @@ import ttf from './icon-font/kilo-icons.ttf';
 import eot from './icon-font/kilo-icons.eot';
 import woff from './icon-font/kilo-icons.woff';
 import woff2 from './icon-font/kilo-icons.woff2';
+import ligaturejson from './liga-font/kilo-icons.json';
+import ligaturecss from './liga-font/kilo-icons.css';
+import ligaturettf from './liga-font/kilo-icons.ttf';
+import ligatureeot from './liga-font/kilo-icons.eot';
+import ligaturewoff from './liga-font/kilo-icons.woff';
+import ligaturewoff2 from './liga-font/kilo-icons.woff2';
 
 export {
   json as kiloIconJSON,
@@ -12,4 +18,10 @@ export {
   eot as kiloIconEOT,
   woff as kiloIconWOFF,
   woff2 as kiloIconWOFF2,
+  ligaturejson as kiloLigaIconJSON,
+  ligaturecss as kiloLigaIconCSS,
+  ligaturettf as kiloLigaIconTTF,
+  ligatureeot as kiloLigaIconEOT,
+  ligaturewoff as kiloLigaIconWOFF,
+  ligaturewoff2 as kiloLigaIconWOFF2,
 };
